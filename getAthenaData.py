35e3ed7560cb9dbbf92e73e21271ce0b7ea158a4
Y Minh Le <y.minhle@symphony.com>
2020-07-08 18:28:00 +0700

    get all table in pages

--- getAthenaData.py
+++ getAthenaData.py
@@ -18,14 +18,20 @@ browser = webdriver.Chrome(executable_path=CHROME_DRIVER_PATH, chrome_options=ch
 browser.get(link)
 delay = 20
 
-tb = browser.find_element(By.ID, "statisticsTable")
-if tb is None:
-    print("not existed")
-else:
-    rows = tb.find_elements(By.TAG_NAME,"tr")
-    for row in rows:        
-        cols = row.find_elements(By.TAG_NAME,"td")
-        if (len(cols) > 0):
-            print(cols[0].text)        
+
+tbs = browser.find_elements(By.TAG_NAME, "table")
+
+for tb in tbs:
+    print(tb.get_attribute("id"))
+
+# tb = browser.find_element(By.ID, "statisticsTable")
+# if tb is None:
+#     print("not existed")
+# else:
+#     rows = tb.find_elements(By.TAG_NAME,"tr")
+#     for row in rows:        
+#         cols = row.find_elements(By.TAG_NAME,"td")
+#         if (len(cols) > 0):
+#             print(cols[0].text)        
     
 print(browser.title)
