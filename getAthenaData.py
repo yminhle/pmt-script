017159ab4fce496e130048cf56b7dc10760d6064
Y Minh Le <y.minhle@symphony.com>
2020-07-08 17:46:17 +0700

    update code

--- getAthenaData.py
+++ getAthenaData.py
@@ -1,7 +1,4 @@
 
-import urllib.request
-import time
-
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.chrome.options import Options
